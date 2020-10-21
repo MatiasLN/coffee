4aa983be8e164413765a1a28de06dd130bff5fa8
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-10-21 12:45:30 +0200

    components

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Covfefe Coffee</h1>
+      <h2>Kaffeliste</h2>
+      <p>En liste over alle typer kaffe som er testet.</p>
+    </div>
+  );
+};
+
+export default Title;
