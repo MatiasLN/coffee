ea805ea8ed5fbf24cce557d925765cc200fa951c
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-10-24 02:04:49 +0200

    removed subtitle

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,6 @@ const Title = () => {
     <div className="title">
       <h1>Covfefe Coffee</h1>
       <h2>Kaffeliste</h2>
-      <p>En liste over alle typer kaffe som er testet.</p>
     </div>
   );
 };
