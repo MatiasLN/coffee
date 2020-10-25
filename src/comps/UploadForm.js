b9b34d22e4c891d88528dfe7dd82685759094875
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-10-26 00:14:33 +0100

    added rating to form

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import ProgressBar from "./ProgressBar";
+import StarRating from "./StarRating";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [title, setTitle] = useState("");
   const [error, setError] = useState(null);
   const types = ["image/png", "image/jpg", "image/jpeg", "image/heic"];
+  const [newRating, setRating] = useState(0);
 
   const changeHandlerText = (e) => {
     setTitle(e.target.value);
@@ -22,25 +24,44 @@ const UploadForm = () => {
     }
   };
 
+  const handleSetRating = (newRating) => {
+    setRating(newRating);
+    localStorage.setItem("rating", newRating);
+  };
+
   return (
-    <form>
-      <input
-        id="file-title"
-        type="text"
-        placeholder="Hva heter kaffetypen?"
-        onChange={changeHandlerText}
-        value={title}
-      />
-      <label htmlFor="file-upload" className="custom-file-upload">
-        Legg til bilde
-      </label>
-      <input id="file-upload" type="file" onChange={changeHandler} />
-      <div className="output">
-        {error && <div className="error">{error}</div>}
-        {file && <div>Legger til {title} i databasen</div>}
-        {file && <ProgressBar file={file} setFile={setFile} />}
-      </div>
-    </form>
+    <>
+      <button
+        className="addNewBtn"
+        onClick={(e) => {
+          document.querySelector("form").style.display = "flex";
+        }}
+      >
+        Legg til ny kaffetype
+      </button>
+      <form style={{ display: "none" }}>
+        <input
+          id="file-title"
+          type="text"
+          placeholder="Hva heter kaffetypen?"
+          onChange={changeHandlerText}
+          value={title}
+        />
+        <input id="file-notes" type="textarea" placeholder="Smaksnotater ..." />
+        <div className="starRating">
+          <StarRating rating={newRating} setRating={handleSetRating} />
+        </div>
+        <label htmlFor="file-upload" className="custom-file-upload">
+          Legg til bilde
+        </label>
+        <input id="file-upload" type="file" onChange={changeHandler} />
+        <div className="output">
+          {error && <div className="error">{error}</div>}
+          {file && <div>Legger til {title} i databasen</div>}
+          {file && <ProgressBar file={file} setFile={setFile} />}
+        </div>
+      </form>
+    </>
   );
 };
 export default UploadForm;
