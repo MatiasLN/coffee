db9caaafefe957b94c93fc0a2a20cae377a88294
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-10-27 21:12:51 +0100

    Only show real pictures on deploy

--- src/comps/ImageItem.js
+++ src/comps/ImageItem.js
@@ -15,6 +15,16 @@ const ImageItem = ({ setSelectedImg, doc }) => {
     collectionRef.update({ star: rating });
   };
 
+  const Image = () => {
+    const LOCAL_DOMAINS = ["localhost", "127.0.0.1"];
+
+    if (LOCAL_DOMAINS.includes(window.location.hostname)) {
+      return <img src={placeholderImg} alt={doc.id} />;
+    } else {
+      return <img src={doc.url} alt={doc.id} />;
+    }
+  };
+
   return (
     <div className="coffeItem">
       <div
@@ -25,11 +35,7 @@ const ImageItem = ({ setSelectedImg, doc }) => {
           setCurrentId(doc.id);
         }}
       >
-        {useLocalhost ? (
-          <img src={placeholderImg} alt={doc.id} />
-        ) : (
-          <img src={doc.url} alt={doc.id} />
-        )}
+        {Image()}
       </div>
       <h2>{doc.title}</h2>
       <div
