936b2b915958cb2074b5029dabd721f31141cdc6
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-10-27 18:20:54 +0100

    remove unused state

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -9,7 +9,6 @@ const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState(null);
   const [url, setUrl] = useState(null);
-  const [isActive, setIsActive] = useState({ display: "none" });
 
   useEffect(() => {
     // ref
