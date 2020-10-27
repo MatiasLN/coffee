fbebde230b648338b55779e829d3b82db0eac755
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-10-27 14:10:39 +0100

    Updates

---
+++ precache-manifest.63ecb83db0f780fbdb894644f507cd98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3679c0ac9ce5d2d646cb131b1b4d1a0",
+    "url": "/coffee/index.html"
+  },
+  {
+    "revision": "a74c708f864ed50b563b",
+    "url": "/coffee/static/css/main.331a8f75.chunk.css"
+  },
+  {
+    "revision": "c8e89ce1b799909dd9c1",
+    "url": "/coffee/static/js/2.c9f8ea3b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/coffee/static/js/2.c9f8ea3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a74c708f864ed50b563b",
+    "url": "/coffee/static/js/main.39effcd1.chunk.js"
+  },
+  {
+    "revision": "e117c09901fa6bdb766b",
+    "url": "/coffee/static/js/runtime-main.ec6cf7ee.js"
+  },
+  {
+    "revision": "505dce53efcf88edce7688c08d2c06a8",
+    "url": "/coffee/static/media/placeholder.505dce53.jpeg"
+  }
+]);
