5ce35a60a56c08858c7b5d218c8a19308ea0bf8d
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-10-28 19:36:22 +0100

    Added notes to modal

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import StarRating from "./StarRating";
 import { projectFirestore } from "../firebase/config";
 import Image from "./Image";
+import Notes from "./Notes";
 
 const Modal = ({ data, setData }) => {
   const [rating, setRating] = useState(data.star);
+  const [notes, setNotes] = useState(data.notes);
   const handleClick = (e) => {
     if (e.target.classList.contains("backdrop")) {
       setData(null);
       window.location.reload(false);
     }
+  };
+
+  const changeHandlerTextarea = (e) => {
+    setNotes(e.target.value);
+
+    const collectionRef = projectFirestore.collection("images").doc(data.id);
+    collectionRef.update({ notes: notes });
   };
 
   const handleSetRating = (rating) => {
@@ -29,6 +38,14 @@ const Modal = ({ data, setData }) => {
         )}
         <div className="modal-content">
           {data.title && <h2>{data.title}</h2>}
+          <textarea
+            id="file-notes"
+            rows={5}
+            cols={5}
+            placeholder="Smaksnotater ..."
+            value={notes}
+            onChange={changeHandlerTextarea}
+          />
           {data.star && (
             <div className="modal-stars">
               {<StarRating rating={rating} setRating={handleSetRating} />}
