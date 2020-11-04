6c343ec1df5aae7608ede2f5b6c3f5aa3b42f5aa
Matias Lie-Nielsen <matiaslienielsen@gmail.com>
2020-11-04 02:23:54 +0100

    added nav menu & updated user page

--- src/comps/User.js
+++ src/comps/User.js
@@ -1,21 +1,39 @@
 import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import { logOut } from "../firebase/config";
 
 const User = () => {
   let user = useContext(UserContext);
   user = user.user;
 
+  const navigation = () => {
+    document.querySelector(".hamburger").classList.toggle("is-active");
+    document.querySelector(".userPage").classList.toggle("show");
+    document.querySelector("nav").classList.toggle("active");
+  };
+
   return (
     <>
+      <nav>
+        <button
+          className="hamburger hamburger--slider"
+          type="button"
+          onClick={navigation}
+        >
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+      </nav>
+
       {user ? (
         <>
-          <div className="userIsLoggedIn">
-            <img src={user.photoURL} alt={user.photoURL} />
-            <p>Hei {user.displayName}</p>
-          </div>
-
           <div className="userPage">
-            <p>Hello</p>
+            <div className="userContent">
+              <img src={user.photoURL} alt={user.photoURL} />
+              <p>Hei {user.displayName}</p>
+              <button className="logOutBtn">Logg ut</button>
+            </div>
           </div>
         </>
       ) : null}
